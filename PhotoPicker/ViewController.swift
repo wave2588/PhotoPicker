689c2c2cd7ddd7558904363d706ed98e94b9ea92
wave <zhanghaibo@bxzz.net>
2018-11-26 10:25:56 +0800

    Initial Commit

---
+++ PhotoPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoPicker
+//
+//  Created by wave on 2018/11/26.
+//  Copyright © 2018 wave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
